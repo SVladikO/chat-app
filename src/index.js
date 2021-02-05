55bd26c57de4ca79687fa2cf7c366aaa090c23c0
svladiko <svladiko@mail.ru>
2021-02-05 09:04:14 +0200

    Broadcating Event. Join & Left user. Lesson 157

--- src/index.js
+++ src/index.js
@@ -18,9 +18,14 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'welcome')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
